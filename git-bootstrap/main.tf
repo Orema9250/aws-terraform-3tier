5a631f22c8edfdb66a0b7db5bffeed056e5ca569
Orema9250 <orema9250@gmail.com>
2026-08-20 11:53:41 +0100

    terrform workflow

--- git-bootstrap/main.tf
+++ git-bootstrap/main.tf
@@ -8,65 +8,6 @@ resource "aws_iam_openid_connect_provider" "github_action" {
   client_id_list = ["sts.amazonaws.com"]
 
 }
-resource "aws_iam_role" "github_action_role" {
-  name_prefix = "githubrole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRoleWithWebIdentity"
-        Effect = "Allow"
-        Principal = { 
-          Federated = aws_iam_openid_connect_provider.github_action.arn
-        }
-       
-        Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-
-            "token.actions.githubusercontent.com:sub" = "repo:Orema9250@242164193/aws-terraform-3tier@1335039067:ref:refs/heads/main"
-          }
-        }
-      
-        }
-    
-    ]
-})
-}  
-resource "aws_iam_role_policy" "github_ecr" {
-  role = aws_iam_role.github_action_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-
-    Statement = [
-      {
-        Effect = "Allow"
-
-        Action = [
-          "ecr:GetAuthorizationToken"
-        ]
-
-        Resource = "*"
-      },
-
-      {
-        Effect = "Allow"
-
-        Action = [
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:CompleteLayerUpload",
-          "ecr:InitiateLayerUpload",
-          "ecr:PutImage",
-          "ecr:UploadLayerPart"
-        ]
-
-        Resource = "arn:aws:ecr:us-east-1:556173312932:repository/backend"
-      }
-    ]
-  })
-}
 
 resource "aws_iam_role" "terraform_github_role" {
   name_prefix = "githubterraformrole"
@@ -101,6 +42,32 @@ resource "aws_iam_role_policy" "github_terraform_role_policy" {
     policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
+      {
+      Sid = "ecrauthorizzationpolicy"
+         Effect = "Allow"
+
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+
+        Resource = "*"
+      },
+
+      {
+        Sid = "ecrpushpolicy"
+        Effect = "Allow"
+
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:CompleteLayerUpload",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ]
+
+        Resource = "arn:aws:ecr:us-east-1:556173312932:repository/backend"
+      },
+    
       {
         Sid = "TerrarformStateBucket"
         Action = [
