6afd88aa12c735e620219a0529fd813294baf275
Orema9250 <orema9250@gmail.com>
2026-08-21 10:14:54 +0100

    terraform fmt

--- git-bootstrap/main.tf
+++ git-bootstrap/main.tf
@@ -162,7 +162,7 @@ resource "aws_iam_role_policy" "github_terraform_role_policy" {
         Sid = "CreateSNStopic"
         Action = [
           "sns:CreateTopic", "sns:DeleteTopic", "sns:ListTopics", "sns:ConfirmSubscription",
-          "sns:GetSubscriptionAttributes", "SNS:SetTopicAttributes", "SNS:GetTopicAttributes", 
+          "sns:GetSubscriptionAttributes", "SNS:SetTopicAttributes", "SNS:GetTopicAttributes",
         ]
         Effect   = "Allow"
         Resource = "*"
